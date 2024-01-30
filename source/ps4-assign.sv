8ebb5ff2072273e0430cc910f25963b94911c777
zc2737 <zc2737@remote01.ee.columbia.edu>
2024-01-30 14:10:04 -0500

    finished ps4-assign.sv

--- source/ps4-assign.sv
+++ source/ps4-assign.sv
@@ -10,5 +10,10 @@ module ps4 (
     // ex:
     // assign x = y[1] & z & ~a;
 
+    assign gnt[3] = en & req[3];
+    assign gnt[2] = en & req[2] & ~req[3];
+    assign gnt[1] = en & req[1] & ~req[2] & ~req[3];
+    assign gnt[0] = en & req[0] & ~req[1] & ~req[2] & ~req[3];
+
 
 endmodule
