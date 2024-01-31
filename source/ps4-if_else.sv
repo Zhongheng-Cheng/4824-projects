e5ac87056b63d3467109cc9d4649a342bc57f9ac
zc2737 <zc2737@remote01.ee.columbia.edu>
2024-01-30 20:51:47 -0500

    finished ps4-if_else.sv

--- source/ps4-if_else.sv
+++ source/ps4-if_else.sv
@@ -20,5 +20,19 @@ module ps4 (
     //     end
     // end
 
+    always_comb begin
+        gnt = 4'b0000;
+        if (en) begin
+            if (req[3]) 
+                gnt = 4'b1000;
+            else if (req[2] & ~req[3]) 
+                gnt = 4'b0100;
+            else if (req[1] & ~req[2] & ~req[3]) 
+                gnt = 4'b0010;
+            else if (req[0] & ~req[1] & ~req[2] & ~req[3]) 
+                gnt = 4'b0001;
+        end
+    end
+
 
 endmodule
