06a4b55adcfd748863bced1433a46512f6528507
Zhongheng-Cheng <zc2737@columbia.edu>
2024-02-05 20:02:59 -0500

    removed comments

--- project1/source/ps4-assign.sv
+++ project1/source/ps4-assign.sv
@@ -5,11 +5,6 @@ module ps4 (
     output logic [3:0] gnt
 );
 
-    // P1 TODO: use assign statements and logic to create a priority selector
-    // do not use any always blocks
-    // ex:
-    // assign x = y[1] & z & ~a;
-
     assign gnt[3] = en & req[3];
     assign gnt[2] = en & req[2] & ~req[3];
     assign gnt[1] = en & req[1] & ~req[2] & ~req[3];
