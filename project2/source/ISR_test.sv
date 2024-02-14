e9d4ab18dd06aca32ded427c5a7f5919a7f2b7e7
Zhongheng-Cheng <zc2737@columbia.edu>
2024-02-13 21:30:17 -0500

    added specific ground_truth calculating

--- project2/source/ISR_test.sv
+++ project2/source/ISR_test.sv
@@ -3,13 +3,15 @@ module testbench();
 	logic [63:0] val;
 	logic clock, reset, done;
 	logic [31:0] result;
+	logic [31:0] ground_truth;
 
     // P2 NOTE: Constructing a correct result can be difficult for the ISR
     //          it can be easier to just check two things:
     //          - result isn't too large: result*result <=value
     //          - result isn't too small: (result+1)*(result+1) > value
-    logic check = (result * result <= val) && ((result + 1) * (result + 1) > val);
-    logic correct = ~done || (check);
+    // logic check = (result * result <= val) && ((result + 1) * (result + 1) > val);
+    // logic correct = ~done || (check);
+	wire correct = (ground_truth===result)|~done|reset;
 
     ISR isr(
         .reset(reset), 
@@ -30,6 +32,19 @@ module testbench();
 		clock=~clock;
 	end
 
+	task cal_ground_truth;
+        input [63:0] value;
+        output [31:0] ground_truth;
+        begin
+            integer j;
+			ground_truth = 32'h00000000;
+            for (j=31; j >= 0; j--) begin
+				ground_truth[j] = 1'b1;
+				if (ground_truth * ground_truth > value) ground_truth[j] = 1'b0;
+            end
+        end
+    endtask
+
 	task wait_until_done;
 		forever begin : wait_loop
 			@(posedge done);
@@ -44,6 +59,7 @@ module testbench();
         input [63:0] value;
         val = value;
 		reset = 1;
+		cal_ground_truth(val, ground_truth);
         clock = 0;
 		@(negedge clock);
 		reset = 0;
@@ -67,7 +83,7 @@ module testbench();
 
         // test on random values
         for (integer i = 0; i < 1000; i++) begin
-			test_value($random);
+			test_value({$random, $random});
 		end
         
 		$display("@@@Passed");
@@ -75,3 +91,6 @@ module testbench();
 	end
 
 endmodule
+
+
+
